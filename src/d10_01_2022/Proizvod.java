0810fdb491582cd20c3f0e07160168dfaf0b1c9a
Initram89 <goran_sb@yahoo.com>
2022-01-12 09:17:43 +0100

    Zadatak1 - update

--- src/d10_01_2022/Proizvod.java
+++ src/d10_01_2022/Proizvod.java
@@ -7,9 +7,27 @@ public class Proizvod {
 	double tezina;
 
 	public void stampa() {
-
-		System.out.println("{{" + this.naziv + "}}, {{" + this.cena + "}}, {{" + this.tezina + "}}");
-
+		System.out.println(this.naziv + ", " + this.cena + ", " + this.tezina);
 	}
 
+	public void povecajCenu(double povecanje) {
+		this.cena = this.cena + povecanje;
+	}
+
+	public double vratiCenuSaPopustom(int popust) {
+		double vratiCenuSaPopustom = this.cena * (100 - popust) / 100;
+		return vratiCenuSaPopustom;
+	}
+
+	public int postarina() {
+		int postarina;
+		if (this.tezina <= 100) {
+			postarina = 200;
+		} else if (this.tezina < 500) {
+			postarina = 400;
+		} else {
+			postarina = 1000;
+		}
+		return postarina;
+	}
 }
--- src/d10_01_2022/Zadatak1.java
+++ src/d10_01_2022/Zadatak1.java
@@ -5,14 +5,14 @@ public class Zadatak1 {
 	public static void main(String[] args) {
 
 		Proizvod kafaGrand = new Proizvod();
-		kafaGrand.naziv = "Gran Kafa";
+		kafaGrand.naziv = "Grand Kafa";
 		kafaGrand.cena = 109.99;
 		kafaGrand.tezina = 100;
 
 		Proizvod milkaJagoda = new Proizvod();
 		milkaJagoda.naziv = "Milka Jagoda";
-		milkaJagoda.cena = 125.49;
-		milkaJagoda.tezina = 90;
+		milkaJagoda.cena = 275.49;
+		milkaJagoda.tezina = 250;
 
 		Proizvod secer = new Proizvod();
 		secer.naziv = "Secer kristal";
@@ -23,6 +23,20 @@ public class Zadatak1 {
 		milkaJagoda.stampa();
 		secer.stampa();
 
+		
+		kafaGrand.povecajCenu(9.99);
+		milkaJagoda.povecajCenu(5.49);
+		secer.povecajCenu(19.99);
+		
+		System.out.println(kafaGrand.naziv + " sa popustom je " + kafaGrand.vratiCenuSaPopustom(25));
+		System.out.println(milkaJagoda.naziv + " sa popustom je " + milkaJagoda.vratiCenuSaPopustom(15));
+		System.out.println(secer.naziv + " sa popustom je " + secer.vratiCenuSaPopustom(10));
+		
+		System.out.println("Postarina je: " + kafaGrand.postarina() + " din");
+		System.out.println("Postarina je: " + milkaJagoda.postarina() + " din");
+		System.out.println("Postarina je: " + secer.postarina() + " din");
+		
+		
 	}
 
 }
